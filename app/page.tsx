96ad2e983bed9c10b3a3317b8ba0590d92cfd657
Giuseppe Funicello <g.funicello@gmail.com>
2023-05-10 16:29:39 +0100

    somma2

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 export default function Home() {
   const [count, setCount] = useState(0);
   console.log("somma");
+  console.log("somma2");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="text-xl">Hello world!</div>
