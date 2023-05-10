77c56e8135dee970bfff708e9f0d370f2f98ea7a
Giuseppe Funicello <g.funicello@gmail.com>
2023-05-10 15:23:36 +0100

    test2

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(0);
-  console.log("test");
+  console.log("test1");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="text-xl">Hello world!</div>
